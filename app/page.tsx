e23dce75d99b94349a47a91b130c61ef1b9b0a1b
Mac Sweeny <macsweenydev@gmail.com>
2024-10-27 17:10:13 +1000

    release date

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, CreditCard, Database } from "lucide-react";
+import { Terminal } from "./(dashboard)/terminal";
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Macs Cool Ecommerce PnL System</h1>
+      <div>This is a work in progess. Expected MVP Release: Sat 9th Nov</div>
+    </main>
+  );
+}
